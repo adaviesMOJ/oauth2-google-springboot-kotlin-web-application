7cc2bf620107e991cce8811374f06a749db4de6c
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2024-10-18 15:50:34 +0100

    Stage 4 Part 2 - Finalise UI
    In this commit I finalise the current implementation of this oauth2 project.
    We have now got a working example of handling users through oauth2, blocking
    resources from being returned if they don't belong to the current user
    and event some basic UI which changes depending on the user being authenticated.

--- src/main/kotlin/com/learning/oauth/controller/HomeController.kt
+++ src/main/kotlin/com/learning/oauth/controller/HomeController.kt
@@ -8,8 +8,6 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
-// TODO: More pages for each resource (Home, User, Cashcards, Error handling, and style with CSS).
-
 @Controller
 @RequestMapping("/", name = "Home Controller", produces = [MediaType.APPLICATION_JSON_VALUE])
 class HomeController {
@@ -17,7 +15,7 @@ class HomeController {
     @GetMapping
     fun getHome(model: Model, @CurrentUser oauth2Identifier: String): String {
         model.addAttribute("oauth2Identifier", oauth2Identifier)
-        return "index"
+        return "home"
     }
 
     @GetMapping("login")
